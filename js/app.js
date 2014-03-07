e3bbb558efb549592aa908eb9a480b71828fdbad
alexandr-deineko <alexandr.deineko@gmail.com>
2014-03-07 18:08:18 +0400

    Require js config

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-/* application */
+/**
+ * Require js global configuration
+ */
+requirejs.config({
+    baseUrl: 'js/',
+
+    paths: {
+        jquery: 'libs/jquery.2.1.0.min',
+        underscore: 'underscore1.6.0.min',
+        backbone: 'backbone.min'
+    }
+});
+
+
+require(
+    ['jquery'],
+    function( $ ){
+        console.log( 'It works!' );
+    }
+);
